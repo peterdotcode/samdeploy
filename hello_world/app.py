64b81925a55898c194c1adcf309f5eb90f3c9de8
unknown <dowling.peter@hotmail.com>
2020-08-23 17:00:29 -0700

    "commiting small change"

--- hello_world/app.py
+++ hello_world/app.py
@@ -37,7 +37,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Hello branching and merging! CI/CD Hello World",
+            "message": "CI/CD Hello World",
             # "location": ip.text.replace("\n", "")
         }),
     }
